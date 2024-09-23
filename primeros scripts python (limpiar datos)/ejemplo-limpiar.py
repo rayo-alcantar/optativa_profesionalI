54fb861a77f771f5b47cf0ed1a81669a71947630
Angel Alcantar <rayoalcantar@gmail.com>
2024-09-23 17:13:31 -0600

    Cargamos los primeros archivos al repo

---
+++ primeros scripts python (limpiar datos)/ejemplo-limpiar.py
@@ -0,0 +1,32 @@
+﻿import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+	'A': [1, 2, np.nan, 4, 5],
+	'B': [5, 6, 7, np.nan, 9],
+	'C': ['a', 'b', 'c', 'd', 'e'],
+	'D': ['foo', 'bar', 'foo', 'bar', 'foo']
+})
+
+print(df)
+df_clean = df.dropna()
+print(f'Dataframe sin filas nulas:\n {df_clean}\n')
+df_filled = df.fillna(value=df['A'].min())
+df_filled = df.fillna(value={'B': df['B'].median()})
+print(df_filled)
+# Mostrar la columna 'D' con duplicados
+print("Columna 'D' con duplicados:")
+print(df['D'])
+
+# Eliminar duplicados
+df_no_duplicates = df.drop_duplicates()
+
+# Mostrar la columna 'D' sin duplicados
+print("\nColumna 'D' sin duplicados:")
+print(df_no_duplicates['D'])
+# Renombrar las columnas
+df_renamed = df.rename(columns={'A': 'Alfa', 'B': 'Beta', 'C': 'Charlie', 'D': 'Delta'})
+
+# Mostrar el DataFrame con las columnas renombradas
+print("DataFrame con columnas renombradas:")
+print(df_renamed)
---
+++ primeros scripts python (limpiar datos)/operaciones-basicas.py
@@ -0,0 +1,28 @@
+﻿#importamos pandas
+#angel alcantar>rayoalcantar@gmail.com.
+
+import pandas as pd
+
+data = {
+	'Nombre': ['Ana', 'Juan', 'María', 'Carlos', 'Sofía'],
+	'Edad': [25, 30, 22, 28, 35],
+	'Ciudad': ['Madrid', 'Barcelona', 'Madrid', 'Valencia', 'Barcelona'],
+	'Puntuación': [85, 92, 78, 95, 88]
+}
+
+# Crear DataFrame
+df = pd.DataFrame(data)
+
+# Mostrar DataFrame
+print(f"DataFrame generado:\n {df}")
+# Filtrar personas mayores de 25 años
+mayores25 = df[df['Edad'] > 25]
+
+# Imprimir el resultado del filtrado
+print(f"Personas mayores de 25 años:\n{mayores25}")
+
+# Agrupar por ciudad y obtener la puntuación máxima en cada ciudad
+agrupacion = mayores25.groupby('Ciudad')['Puntuación'].max()
+
+# Imprimir el resultado de la agrupación
+print(f"Puntuación máxima por ciudad para personas mayores de 25 años:\n{agrupacion}")
