2cbf8ad4658f1ba7181c01878439ab7b60947767
Angel Alcantar <rayoalcantar@gmail.com>
2024-10-01 18:27:05 -0600

    Se aañaden algunas gráficas

--- Gráficas/ejercicio-graficas-avanzadas.py
+++ Gráficas/ejercicio-graficas-avanzadas.py
@@ -1,4 +1,4 @@
-﻿# Autor: Ángel De Jesús Alcántar
+﻿# Autor: Ángel De Jesús Alcántar Garza
 # Descripción: Este script analiza el dataset 'student_performance.csv' que contiene información de estudiantes
 # y realiza diferentes visualizaciones, incluyendo gráficos interactivos y no interactivos.
 # Dataset obtenido desde: https://github.com/rayo-alcantar/optativa_profesionalI/blob/main/datos/student_performance.csv
@@ -25,16 +25,12 @@ df = pd.read_csv(url)
 # - FinalGrade: Nota final del estudiante.
 
 # Gráfico 1: Gráfico de dispersión interactivo - Horas de estudio vs Calificación final
-# En este gráfico visualizamos cómo varían las calificaciones en función de las horas de estudio,
-# con el tamaño del punto representando la cantidad de actividades extracurriculares.
-
 fig_scatter = px.scatter(df, x='StudyHoursPerWeek', y='FinalGrade', size='ExtracurricularActivities', color='Gender',
                          hover_data=['AttendanceRate', 'PreviousGrade'],
                          title="Gráfico de Dispersión Interactivo: Horas de Estudio vs Calificación Final")
 fig_scatter.show()
 
 # Gráfico 2: Gráfico de líneas interactivo - Tasa de asistencia y calificaciones
-# En este gráfico mostramos cómo la tasa de asistencia afecta tanto la calificación final como la calificación previa.
 x_vals = df['AttendanceRate']
 y_vals1 = df['FinalGrade']
 y_vals2 = df['PreviousGrade']
@@ -48,8 +44,6 @@ fig_lines.update_layout(title='Gráfico de Líneas Interactivo: Asistencia vs Ca
 fig_lines.show()
 
 # Gráfico 3: Gráfico de barras - Actividades extracurriculares por estudiante
-# Este gráfico de barras nos muestra la cantidad de actividades extracurriculares que realizan los estudiantes.
-
 plt.figure(figsize=(10, 6))
 sns.barplot(x='Name', y='ExtracurricularActivities', data=df)
 plt.title('Actividades Extracurriculares por Estudiante')
@@ -57,17 +51,27 @@ plt.xticks(rotation=45)
 plt.show()
 
 # Gráfico 4: Gráfico de dispersión - Asistencia vs Calificación Final
-# Este gráfico nos permite ver si existe alguna relación entre la tasa de asistencia y la calificación final.
-
 plt.figure(figsize=(10, 6))
 sns.scatterplot(x='AttendanceRate', y='FinalGrade', hue='Gender', data=df)
 plt.title('Asistencia vs Calificación Final')
 plt.show()
 
 # Gráfico 5: Histograma - Distribución de las calificaciones finales
-# Un histograma que muestra la distribución de las calificaciones finales obtenidas por los estudiantes.
-
 plt.figure(figsize=(10, 6))
 sns.histplot(df['FinalGrade'], kde=True)
 plt.title('Distribución de las Calificaciones Finales')
+plt.show()
+
+# Gráfico 6: Gráfico de caja y brazos - Comparación de calificaciones por género
+# Este gráfico de caja nos ayuda a visualizar la distribución de las calificaciones finales según el género.
+plt.figure(figsize=(10, 6))
+sns.boxplot(x='Gender', y='FinalGrade', data=df)
+plt.title('Box Plot: Calificaciones por Género')
+plt.show()
+
+# Gráfico 7: Gráfico de violín - Distribución de calificaciones finales por nivel de apoyo parental
+# Este gráfico de violín muestra la distribución de las calificaciones finales según el nivel de apoyo parental.
+plt.figure(figsize=(10, 6))
+sns.violinplot(x='ParentalSupport', y='FinalGrade', data=df)
+plt.title('Violin Plot: Calificaciones por Nivel de Apoyo Parental')
 plt.show()
